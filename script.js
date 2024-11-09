fbfe57755f43491db16a3839088a94f013908450
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-11-09 22:07:57 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function openResume() {
-    window.location.href = "resume.html"; // Yeh page open karega jisme tumhara resume hoga
-}
-function openBuilder() {
-    window.location.href = "create-resume.html"; // Yeh page open karega jisme tumhara resume hoga
-}
